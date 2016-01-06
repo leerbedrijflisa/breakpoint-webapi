8da9589f48281173cc66e4a339a08aa1b0da4218
Ramon <ramon.meijers@hotmail.com>
2016-01-06 13:07:08 +0100

    Added ticket #10 and aligned the values

--- Lisa.Breakpoint.WebApi/controllers/ValueController.cs
+++ Lisa.Breakpoint.WebApi/controllers/ValueController.cs
@@ -19,13 +19,32 @@ namespace Lisa.Breakpoint.WebApi.controllers
         [HttpGet("project-roles")]
         public IActionResult ProjectRoles()
         {
-            return new HttpOkObjectResult(new { manager = "Project manager", developer = "Developer", tester = "Tester" });
+            return new HttpOkObjectResult(new {
+                manager = "Project manager",
+                developer = "Developer",
+                tester = "Tester"
+            });
         }
 
         [HttpGet("statuses")]
         public IActionResult Statuses()
         {
-            return new HttpOkObjectResult(new { open = "Open", @fixed = "Fixed", wontFix = "Won't Fix", wontFixApproved = "Won't Fix (Approved)", closed = "Closed" });
+            return new HttpOkObjectResult(new {
+                open = "Open",
+                @fixed = "Fixed",
+                wontFix = "Won't Fix",
+                wontFixApproved = "Won't Fix (Approved)",
+                closed = "Closed"
+            });
+        }
+
+        [HttpGet("organization-roles")]
+        public IActionResult OrganizationRoles()
+        {
+            return new HttpOkObjectResult(new {
+                manager = "Organization manager",
+                member = "Member"
+            });
         }
     }
 }
